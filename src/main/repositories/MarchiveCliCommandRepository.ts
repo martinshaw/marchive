a09feca12fa02f58290c206c44810a8456f29fc3
Martin Shaw <developer@martinshaw.co>
2023-08-30 23:22:28 +0100

    Removed marchive-cli package. Commit before creating branch which integrates marchive-cli functionality instead of making calls to cli package

--- src/main/repositories/MarchiveCliCommandRepository.ts
+++ src/main/repositories/MarchiveCliCommandRepository.ts
@@ -9,11 +9,38 @@ Modified: 2023-08-29T18:36:41.163Z
 Description: description
 */
 
+import {spawn} from 'child_process'
+import path from 'node:path';
+import { rootPath } from '../../paths';
+
 export const runMarchiveCliCommand = async (
-  command: string,
   args: string[],
 ): Promise<string> => {
-  return ''
+  return new Promise((resolve, reject) => {
+    const cliPath = path.join(rootPath, 'node_modules', '@martinshaw', 'marchive-cli', 'bin', 'run')
+    const cliArguments = [...args, '--json']
+
+    const childProcess = spawn(cliPath, cliArguments)
+
+    let stdout = ''
+    let stderr = ''
+
+    childProcess.stdout.on('data', (data) => {
+      stdout += data
+    })
+
+    childProcess.stderr.on('data', (data) => {
+      stderr += data
+    })
+
+    childProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve(stdout)
+      } else {
+        reject(stderr)
+      }
+    })
+  })
 }
 
 export default () => {};
---
+++ src/paths.ts
@@ -0,0 +1,14 @@
+/*
+All Rights Reserved, (c) 2023 CodeAtlas LTD.
+
+Author: Martin Shaw (developer@martinshaw.co)
+File Name: paths.ts
+Created:  2023-08-22T20:33:52.038Z
+Modified: 2023-08-22T20:33:52.039Z
+
+Description: description
+*/
+
+import path from 'node:path'
+
+export const rootPath = path.join(__dirname, '..')
