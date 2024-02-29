fe98ba7fb486d625388fb54f60fc539fab3f743f
Martin Shaw <developer@martinshaw.co>
2024-02-29 11:26:19 +0000

    Make styling consistent, finish implementing UI for image file download DP

--- packages/electron-app/src/renderer/layouts/DefaultLayout/hooks/useGetObjectFromJsonFile.ts
+++ packages/electron-app/src/renderer/layouts/DefaultLayout/hooks/useGetObjectFromJsonFile.ts
@@ -9,36 +9,34 @@ Modified: 2023-09-15T20:38:58.177Z
 Description: description
 */
 
-// import { JSONObject } from "types-json";
-import { useAsyncMemo } from "use-async-memo";
+import { JSONObject } from 'types-json';
+import { useAsyncMemo } from 'use-async-memo';
 
-// export type UseGetObjectFromJsonFileReturnType = JSONObject | null | undefined
-export type UseGetObjectFromJsonFileReturnType = any | null | undefined
+export type UseGetObjectFromJsonFileReturnType = JSONObject | null | undefined;
 export type UseGetObjectFromJsonFilePropsType = {
   if: boolean;
   filePath: string;
 };
 
-const useGetObjectFromJsonFile: (props: UseGetObjectFromJsonFilePropsType) => UseGetObjectFromJsonFileReturnType = (props) => {
-  // const jsonObject = useAsyncMemo<JSONObject | null | undefined>(
-  const jsonObject = useAsyncMemo<any | null | undefined>(
+const useGetObjectFromJsonFile: (
+  props: UseGetObjectFromJsonFilePropsType,
+) => UseGetObjectFromJsonFileReturnType = (props) => {
+  const jsonObject = useAsyncMemo<JSONObject | null | undefined>(
     async () =>
-      props.if ?
-        fetch(props.filePath)
-          .then(response => response.json())
-          // .then(data => data as JSONObject)
-          .then(data => data as any)
-          .catch(error => {
-            console.log(error)
-            return null
-          }) :
-        null
-    ,
+      props.if
+        ? fetch(props.filePath)
+            .then((response) => response.json())
+            .then((data) => data as JSONObject)
+            .catch((error) => {
+              console.log(error);
+              return null;
+            })
+        : null,
     [props.if, props.filePath],
     null,
-  )
+  );
 
-  return jsonObject
+  return jsonObject;
 };
 
 export default useGetObjectFromJsonFile;
